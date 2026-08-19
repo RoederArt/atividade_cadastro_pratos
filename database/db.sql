a604039c687bad374ca100b08d37df98f22aa13a
Gustavo_Fenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-08-19 11:25:16 -0300

    ref - melhores o banco de dados

--- database/db.sql
+++ database/db.sql
@@ -4,15 +4,18 @@ use cadastro_pratos;
 
 CREATE TABLE pratos(
     id_pratos INT PRIMARY KEY AUTO_INCREMENT,
-    nome_pratos VARCHAR(40) NOT NULL,
+    nome_pratos VARCHAR(100) NOT NULL,
     descrição_pratos TEXT NOT NULL,
-    preço_pratos INT NOT NULL,
-    categoria_pratos VARCHAR(40) NOT NULL
+    preço_pratos DECIMAL(10,2) NOT NULL,
+    categoria_pratos VARCHAR(100) NOT NULL
+
+    id_usuario int not null,
+    foreign key (id_usuario) references usuarios(id_usuario)
 );
 
 CREATE TABLE usuarios(
         id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-        nome_usuario VARCHAR(40) NOT NULL,
-        email_usuario VARCHAR(40) NOT NULL
+        nome_usuario VARCHAR(100) NOT NULL,
+        email_usuario VARCHAR(100) NOT NULL
 );
 
